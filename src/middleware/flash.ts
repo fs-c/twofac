fcc5effff538a30ed46e317e107885641e0832fb
fsoc <8392143a@gmail.com>
2018-02-10 18:49:55 +0100

    Implement flash messages.

---
+++ src/middleware/flash.ts
@@ -0,0 +1,24 @@
+import { Context } from 'koa';
+import * as logger from 'debug';
+
+const debug = logger('flash');
+
+export default function flash() {
+  return (ctx: Context, next: () => Promise<any>) => {
+    const prev = ctx.session.flash;
+
+    if (prev) {
+      debug('message found %j', prev);
+      ctx.session.flash = null;
+    } else {
+      debug('no message found');
+    }
+
+    ctx.flash = {
+      get: () => prev,
+      set: (data: any) => ctx.session.flash = data,
+    };
+
+    return next();
+  };
+}
