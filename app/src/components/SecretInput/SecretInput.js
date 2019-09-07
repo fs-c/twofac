f01ff055c147843537cf1ed5ea1e355b53f9cfa3
lw <root@fsoc.space>
2019-09-07 19:22:07 +0200

    Remove unused import

--- app/src/components/SecretInput/SecretInput.js
+++ app/src/components/SecretInput/SecretInput.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
