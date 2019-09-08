98f0e52ae8a04f40274a8fe7d98f68fe8e99fcc7
lw <root@fsoc.space>
2019-09-08 22:22:44 +0200

    Move inline styles into CSS

--- app/src/components/CodeList/CodeList.js
+++ app/src/components/CodeList/CodeList.js
@@ -4,16 +4,6 @@ import Alert from 'react-bootstrap/Alert';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 const CodeList = ({ liveCode, remainingTime, codes }) => {
-    // This could be moved to a .css file, but it's kept around in here in 
-    // case the idea of communicating the remaining time through styling is 
-    // ever relevant again
-    const borderStyle = {
-        background: 'inherit',
-        borderBottom: '1.5px solid transparent',
-        borderImage: `linear-gradient(90deg, black 20%, #6c757d 100%)`,
-        borderImageSlice: '1',
-    };
-
     return (<>
         {(liveCode && liveCode.current) && (
             <Alert variant='dark' className='bg-dark text-light border-light'>
@@ -35,9 +25,7 @@ const CodeList = ({ liveCode, remainingTime, codes }) => {
         {(codes && codes.length) && (
             <ListGroup variant='flush' className='mt-4 rounded border'>
                 {codes.map((c) => (
-                    <ListGroup.Item key={c.secret} className='text-light'
-                        style={borderStyle}
-                    >
+                    <ListGroup.Item key={c.secret} className='text-light bg-inherit list-border-transition'>
                         <span className='text-muted'>{c.alias}</span><br/>
 
                         <div className='h5 steam-code'>
