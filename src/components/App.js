3982430275d9a7b93c1007910c87833af8efcfc3
lw <root@fsoc.space>
2019-10-22 21:42:16 +0200

    Add basic routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
-const App = () => (
+import { Switch, Route } from 'wouter';
+
+const Home = () => (
     <p>
         Hello world
     </p>
+);
+
+const What = () => (
+    <p>
+        What?
+    </p>
+);
+
+const Contact = () => (
+    <p>
+        Contact
+    </p>
+);
+
+const NotFound = () => (
+    <p>
+        Not Found
+    </p>
+);
+
+const App = () => (
+    <Switch>
+        <Route path='/' component={Home} />
+        <Route path='/what' component={What} />
+        <Route path='/contact' component={Contact} />
+
+        <Route path='/:nonexistent*' component={NotFound} />
+    </Switch>
 );
 
 export default App;
