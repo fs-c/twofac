9fc1c73ae7e95d90f29069c33a6048bf9e781266
lw <root@fsoc.space>
2019-10-23 22:51:36 +0200

    Implement saving functionality

--- src/components/SecretInput.js
+++ src/components/SecretInput.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Label from '../components/lib/Label';
 import Input from '../components/lib/Input';
 import Button from '../components/lib/Button';
 import { Flex, VerticalSpacer } from '../components/lib/utils';
 
-const SecretInput = () => {
+import { LocalSecretStore } from '../helpers';
+
+const SecretInput = ({ onSave }) => {
+    const [ alias, setAlias ] = useState('');
+    const [ secret, setSecret ] = useState('');
+
+    const handleSave = () => {
+        if (!alias.length || LocalSecretStore.get(alias)) {
+            // TODO: Communicate this
+            console.error('invalid or duplicate alias');
+
+            return;
+        }
+
+        LocalSecretStore.add(alias, secret);
+
+        onSave({ alias, secret });
+    };
+
     return (
         <>
             <Flex direction='row'>
-                <Input placeholder='Shared Secret' />
+                <Input value={secret} onChange={({ target }) => setSecret(target.value)}
+                    placeholder='Shared Secret' />
             </Flex>
 
             <VerticalSpacer height={3} />
 
             <Flex direction='row'>
-                <Input.Split placeholder='Alias' />
-                <Button.Split>Save</Button.Split>
+                <Input.Split value={alias} onChange={({ target }) => setAlias(target.value)}
+                    placeholder='Alias' />
+                <Button.Split onClick={handleSave}>Save</Button.Split>
             </Flex>
 
             <Label>By default, your secret doesn't leave your browser</Label>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import SecretInput from '../components/SecretInput';
 import { VerticalSpacer } from '../components/lib/utils';
+
+import { LocalSecretStore } from '../helpers';
 
 const links = {
     steamGuard: 'https://support.steampowered.com/kb_article.php?ref=8625-wrah-9030',
 };
 
-const Home = () => (
-    <>
-        <p>
-            A web implementation of the <a href={links.steamGuard}>Steam Guard Mobile 
-            Authenticator</a>.
-        </p>
+const Home = () => {
+    const [ list, setList ] = useState([]);
 
-        <VerticalSpacer height={4} />
+    useEffect(() => {
+        setList(LocalSecretStore.getAll());
+    }, []);
 
-        <SecretInput />
-    </>
-);
+    return (
+        <>
+            <p>
+                A web implementation of the <a href={links.steamGuard}>Steam Guard Mobile 
+                Authenticator</a>.
+            </p>
+
+            <VerticalSpacer height={4} />
+
+            <SecretInput onSave={(item) => setList((prev) => prev.concat([ item ]))} />
+        </>
+    )
+};
 
 export default Home;
