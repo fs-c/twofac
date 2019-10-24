e67339966001bc51ddfd691ddf6d54fe475cb910
lw <root@fsoc.space>
2019-10-24 22:29:48 +0200

    Implement basic code list

--- src/components/CodeList.js
+++ src/components/CodeList.js
@@ -1,13 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import List from '../components/lib/List';
+import { Flex } from '../components/lib/utils';
+import Octicon, { X } from '@primer/octicons-react';
 
-const CodeList = ({ list }) => {
+import styled from 'styled-components';
+import { generateMobileCode } from '../helpers';
+
+const generateCodeTuple = (secret) => ({
+    old: generateMobileCode(secret, -30),
+    current: generateMobileCode(secret),
+});
+
+const CodeList = ({ codes }) => {
+    const Alias = styled.span`
+        color: var(--accents-6);
+    `;
+
+    const ItemRow = styled(Flex).attrs({ direction: 'row' })`
+        justify-content: space-between;
+    `;
+
+    const DeleteButton = styled(Flex)`
+        width: auto;
+        justify-content: space-evenly;
+
+        border: 0px;
+        color: var(--accents-6);
+        background-color: inherit;
+
+        cursor: pointer;
+        transition: 0.2s;
+
+        :hover {
+            color: var(--error);
+        }
+    `;
+
+    const Code = styled.code`
+        font-size: 1.3em;
+        letter-spacing: 1.5px;
+        color: var(--${({ main }) => main ? 'foreground' : 'accents-6'});
+    `;
+
     return (
         <List>
-            {list.map(({ alias }) => (
+            {Object.keys(codes).map((alias) => (
                 <List.Item key={alias}>
-                    {alias}
+                    <ItemRow>
+                        <Alias>{alias}</Alias>
+
+                        <DeleteButton as='button'>
+                            <Octicon icon={X} verticalAlign='middle' />
+                        </DeleteButton>
+                    </ItemRow>
+
+                    <ItemRow>
+                        <div>
+                            <Code main>{codes[alias].current}</Code>
+                        </div>
+
+                        <div>
+                            <Code>{codes[alias].old}</Code>
+                        </div>
+                    </ItemRow>
                 </List.Item>
             ))}
         </List>
@@ -15,8 +71,18 @@ const CodeList = ({ list }) => {
 };
 
 export const UpdatingCodeList = ({ list }) => {
+    const [ codes, setCodes ] = useState({});
+
+    useEffect(() => {
+        setCodes(list.reduce((acc, cur) => {
+            console.log(cur);
+            acc[cur.alias] = generateCodeTuple(cur.secret);
+            return acc;
+        }, {}));
+    }, [ list ]);
+
     return (
-        <CodeList list={list} />
+        <CodeList codes={codes} />
     );
 };
 
